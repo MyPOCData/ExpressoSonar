5931cdf3f7ad294e684dd72e05e6fba6cf53b7ad
abhinavoffice <abhinav.agarwal@wynk.in>
2020-06-05 00:09:13 +0530

    First commit

---
+++ app/src/androidTest/java/com/example/b0097042/expressologin/LoginActivityTest.java
@@ -0,0 +1,165 @@
+package com.example.b0097042.expressologin;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.filters.LargeTest;
+//import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+        ViewInteraction appCompatAutoCompleteTextView = onView(
+                allOf(withId(R.id.email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView2.perform(scrollTo(), replaceText("abc"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatEditText.perform(scrollTo(), replaceText("xyz"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                2)));
+        appCompatButton.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.email), withText("abc"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView3.perform(scrollTo(), click());
+
+        ViewInteraction appCompatAutoCompleteTextView4 = onView(
+                allOf(withId(R.id.email), withText("abc"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatAutoCompleteTextView4.perform(scrollTo(), replaceText("abc@gmail.com"));
+
+        ViewInteraction appCompatAutoCompleteTextView5 = onView(
+                allOf(withId(R.id.email), withText("abc@gmail.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView5.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                2)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password), withText("xyz"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0)));
+        appCompatEditText2.perform(scrollTo(), replaceText("xyz123"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.password), withText("xyz123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                2)));
+        appCompatButton3.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
